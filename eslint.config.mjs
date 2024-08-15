bee4316971a84c85543c0eefaa84a240a04331a0
eanton71 <eanton71@gmail.com>
2024-08-15 11:14:36 +0200

    users

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+
+
+export default [
+ 
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" }
+  },
+  {
+    languageOptions: { globals: globals.node }
+  },
+];
--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 const username = 'eanton71'
-const dbName = 'noteApp'
+const dbName = 'testnoteApp'
 const url =
     `mongodb+srv://${username}:${password}@cluster19070.wvq4dny.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster19070`
 
@@ -28,7 +28,7 @@ noteSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
-/*
+
 let note = new Note({
     content: 'HTML is easy',
     important: true,
@@ -46,32 +46,27 @@ let note3 = new Note({
     content: 'GET and POST are the most important methods of HTTP protocol',
     important: true,
 })
-Promise.all([note.save(),note1.save(), note2.save(), note3.save()])
-    .then(results => {
+// Save all notes in parallel
+Promise.all([note.save(), note1.save(), note2.save(), note3.save()])
+    .then(() => {
         console.log('Notes saved!')
         // Find all notes
-        Note.find({})
-            .then(notes => {
-                notes.forEach(note => {
-                    console.log(note)
-                })
-                mongoose.connection.close()
-            })
-            .catch(error => {
-                console.error('Error finding notes:', error)
-                mongoose.connection.close()
-            })
+        return Note.find({})
+    })
+    .then(notes => {
+        notes.forEach(note => console.log(note))
     })
     .catch(error => {
-        console.error('Error saving notes:', error)
+        console.error('Error saving or finding notes:', error)
+    })
+    .finally(() => {
         mongoose.connection.close()
     })
- 
- */
+/*
 Note.find({}).then(result => {
     result.forEach(note => {
         console.log(note)
     })
     mongoose.connection.close()
-})
+})*/
  
